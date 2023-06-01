8a21c9a9a0ef9d1c2eaadbfe06b9ca5acb4b1ef7
quaq <archk.latham@gmail.com>
2023-05-31 23:10:31 -0500

    deploy

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,10 +2,12 @@
 pragma solidity 0.8.20;
 
 import {Script} from "forge-std/Script.sol";
+import {Swap} from "src/Swap.sol";
 
 contract Deploy is Script {
     function run() external {
         vm.startBroadcast();
+        new Swap();
         vm.stopBroadcast();
     }
 }
